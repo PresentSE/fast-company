e983f48a40c57bcb307c973b8565887493facffe
Sergey Opihaylenko <sergomail@bk.ru>
2022-08-17 22:12:34 +0300

    minor changes

--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -3,25 +3,23 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div>
-            <ul className="nav">
-                <li className="nav-item">
-                    <Link className="nav-link" to={"/"}>
-                        Main
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to={"/login"}>
-                        Login
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to={"/users"}>
-                        Users
-                    </Link>
-                </li>
-            </ul>
-        </div>
+        <ul className="nav">
+            <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/">
+                    Main
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/login">
+                    Login
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/users">
+                    Users
+                </Link>
+            </li>
+        </ul>
     );
 };
 
