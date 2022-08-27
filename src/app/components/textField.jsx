667ca42992e2d51308a87f9e4314e2528d5e2624
Sergey Opihaylenko <sergomail@bk.ru>
2022-08-27 16:00:01 +0300

    add styles to login

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const getInputClasses = () => {
+        return "form-control" + (error ? " is-invalid" : "");
+    };
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
     return (
-        <div>
+        <div className="mb-4">
             <label htmlFor={name}>{label}</label>
-            <input
-                type={type}
-                id={name}
-                name={name}
-                value={value}
-                onChange={onChange}
-            />
-            {error && <p>{error}</p>}
+            <div className="input-group has-validation">
+                <input
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (
+                    <button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                        <i
+                            className={
+                                "bi bi-eye" + (showPassword ? "-slash" : "")
+                            }
+                        ></i>
+                    </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+            </div>
         </div>
     );
 };
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -54,26 +54,37 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label="Электронная почта"
-                name="email"
-                value={data.email}
-                onChange={handleChange}
-                error={errors.email}
-            />
-            <TextField
-                label="Пароль"
-                type="password"
-                name="password"
-                value={data.password}
-                onChange={handleChange}
-                error={errors.password}
-            />
-            <button type="submit" disabled={!isValid}>
-                Submit
-            </button>
-        </form>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Электронная почта"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Пароль"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button
+                            type="submit"
+                            disabled={!isValid}
+                            className="btn btn-primary w-100 mx-auto"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
