0e4e685ed9c21666ed71f02973d3eee98e73c772
Sergey Opihaylenko <sergomail@bk.ru>
2022-10-06 01:12:12 +0300

    edit

--- src/app/components/ui/qualities/qualitieslist.jsx
+++ src/app/components/ui/qualities/qualitieslist.jsx
@@ -2,21 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
 import { useQualities } from "../../../hooks/useQualities";
-import Loader from "../../common/loader";
 
 const QualitiesList = ({ qualities }) => {
-    const { isLoading, getQualities } = useQualities();
-    if (!isLoading) {
-        return (
-            <>
-                {qualities.map((qual) => (
-                    <Quality key={qual._id} {...getQualities(qual)} />
-                ))}
-            </>
-        );
-    } else {
-        return <Loader />;
-    }
+    const { isLoading } = useQualities();
+    if (isLoading) return "Loading...";
+    return (
+        <>
+            {qualities.map((qual) => (
+                <Quality key={qual} id={qual} />
+            ))}
+        </>
+    );
 };
 
 QualitiesList.propTypes = {
--- src/app/components/ui/qualities/quality.jsx
+++ src/app/components/ui/qualities/quality.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-const Quality = ({ color, name, _id }) => {
-    return (
-        <span className={"badge m-1 bg-" + color} key={_id}>
-            {name}
-        </span>
-    );
+import { useQualities } from "../../../hooks/useQualities";
+const Quality = ({ id }) => {
+    const { getQualities } = useQualities();
+    const { color, name } = getQualities(id);
+    return <span className={"badge m-1 bg-" + color}>{name}</span>;
 };
 Quality.propTypes = {
-    color: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired
 };
 
 export default Quality;
