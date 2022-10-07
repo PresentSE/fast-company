45ecc3549ebd04f1df63dd79fd00369ded95c392
Sergey Opihaylenko <sergomail@bk.ru>
2022-10-08 00:54:32 +0300

    edit

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import useMockData from "../utils/mockData";
 
 const Main = () => {
-    return <h1> Main Page</h1>;
+    const { error, initialize, progress, status } = useMockData();
+    const handleClick = () => {
+        console.log("clicked");
+        initialize();
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1> Main Page</h1>
+            <h3>Инициализация данныхз в FireBase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>error: {error} </li>}
+            </ul>
+            <button className="btn btn-primary" onClick={handleClick}>
+                Инициализировать
+            </button>
+        </div>
+    );
 };
 
 export default Main;
