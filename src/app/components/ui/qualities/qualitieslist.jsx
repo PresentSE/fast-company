de40af45a5bb9eb3a91aa54b029672f4399a4b72
Sergey Opihaylenko <sergomail@bk.ru>
2022-10-21 00:25:29 +0300

    edit

--- src/app/components/ui/qualities/qualitieslist.jsx
+++ src/app/components/ui/qualities/qualitieslist.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
 import { useQualities } from "../../../hooks/useQualities";
+import Loader from "../../common/loader";
 
 const QualitiesList = ({ qualities }) => {
     const { isLoading } = useQualities();
-    if (isLoading) return "Loading...";
+    if (isLoading) return <Loader />;
     return (
         <>
             {qualities.map((qual) => (
