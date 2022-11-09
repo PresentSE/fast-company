a38b28278d58f42b6eac37fb4e458c0b8c6ba807
Sergey Opihaylenko <sergomail@bk.ru>
2022-11-10 00:35:01 +0300

    edit

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -5,14 +5,13 @@ import SelectField from "../common/form/selectField";
 import RadioField from "../common/form/radioField";
 import MultiSelectField from "../common/form/multiSelectField";
 import CheckBoxField from "../common/form/checkBoxField";
-import { useAuth } from "../../hooks/useAuth";
-import { useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getQualities } from "../../store/qualities";
 import { getProfessions } from "../../store/professions";
+import { signUp } from "../../store/users";
 
 const RegisterForm = () => {
-    const history = useHistory();
+    const dispatch = useDispatch();
     const [data, setData] = useState({
         email: "",
         password: "",
@@ -22,7 +21,7 @@ const RegisterForm = () => {
         qualities: [],
         licence: false
     });
-    const { signUp } = useAuth();
+
     const qualities = useSelector(getQualities());
     const qualitiesList = qualities.map((q) => ({
         label: q.name,
@@ -96,7 +95,7 @@ const RegisterForm = () => {
     };
     const isValid = Object.keys(errors).length === 0;
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const isValid = validate();
         if (!isValid) return;
@@ -104,12 +103,8 @@ const RegisterForm = () => {
             ...data,
             qualities: data.qualities.map((q) => q.value)
         };
-        try {
-            await signUp(newData);
-            history.push("/");
-        } catch (error) {
-            setErrors(error);
-        }
+        dispatch(signUp(newData));
+        signUp(newData);
     };
 
     return (
--- src/app/store/users.js
+++ src/app/store/users.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import authService from "../services/auth.service";
+import localStorageService from "../services/localStorage.service";
 import userService from "../services/user.service";
 
 const usersSlice = createSlice({
@@ -6,7 +8,9 @@ const usersSlice = createSlice({
     initialState: {
         entities: null,
         isLoading: true,
-        error: null
+        error: null,
+        auth: null,
+        isLoggedIn: false
     },
     reducers: {
         usersRequested: (state) => {
@@ -19,12 +23,39 @@ const usersSlice = createSlice({
         usersRequestFailed: (state, action) => {
             state.error = action.payload;
             state.isLoading = false;
+        },
+        authRequestSuccess: (state, action) => {
+            state.auth = { ...action.payload, isLoggedIn: true };
+        },
+        authRequestFailed: (state, action) => {
+            state.error = action.payload;
         }
     }
 });
 
 const { reducer: usersReducer, actions } = usersSlice;
-const { usersRequested, usersReceived, usersRequestFailed } = actions;
+const {
+    usersRequested,
+    usersReceived,
+    usersRequestFailed,
+    authRequestSuccess,
+    authRequestFailed
+} = actions;
+
+const authRequested = createAction("users/authRequested");
+
+export const signUp =
+    ({ email, password, ...rest }) =>
+    async (dispatch) => {
+        dispatch(authRequested);
+        try {
+            const data = await authService.register({ email, password });
+            localStorageService.setTokens(data);
+            dispatch(authRequestSuccess({ userId: data.localId }));
+        } catch (error) {
+            dispatch(authRequestFailed(error.message));
+        }
+    };
 
 export const loadUsersList = () => async (dispatch, getState) => {
     dispatch(usersRequested());
