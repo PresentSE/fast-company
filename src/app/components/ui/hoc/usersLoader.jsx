fe93dafcdd8a1b03ef6b57ff674c450972f9d9fd
Sergey Opihaylenko <sergomail@bk.ru>
2022-11-13 23:08:46 +0300

    edit

---
+++ src/app/components/ui/hoc/usersLoader.jsx
@@ -0,0 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
+
+
+const UsersLoader = () => {
+   const dataStatus=useSelector(dataStatus())
+   const dispatch=useDispatch()
+    return (  );
+}
+ 
+export default UsersLoader;
